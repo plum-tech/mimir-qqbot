f01d98876842d546903c4ac00089dd8b51161b7e
Liplum <Li_plum@outlook.com>
2024-08-14 08:57:31 +0800

    updated prompt of missing env var

--- r.py
+++ r.py
@@ -11,11 +11,11 @@ else:
 
 appid = os.getenv("QQBOT_APP_ID")
 if appid is None:
-    raise Exception('Missing "MIMIR_QQBOT_APP_ID" environment variable for your bot AppID')
+    raise Exception('Missing "QQBOT_APP_ID" environment variable for your bot AppID')
 
 secret = os.getenv("QQBOT_APP_SECRET")
 if secret is None:
-    raise Exception('Missing "MIMIR_QQBOT_APP_SECRET" environment variable for your AppSecret')
+    raise Exception('Missing "QQBOT_APP_SECRET" environment variable for your AppSecret')
 
 backend_elec = os.getenv("MIMIR_BACKEND_ELEC_URL", default="http://elec.api.mysit.life/v1")
 backend_elec_token = os.getenv("MIMIR_ELEC_ELEVATED_TOKEN")
